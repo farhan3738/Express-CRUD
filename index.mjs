c73f9b9a223ded92c68ce8150d4578f61e46061a
Muhammad Farhan Elahi <mobicare.pew@gmail.com>
2024-09-22 13:08:58 +0500

    Added status code to POST request

--- index.mjs
+++ index.mjs
@@ -12,7 +12,7 @@ let users = [
 app.get('/api/users', (req, res) => {
     res.status(200).json(users);    
   });
-
+// POST REQUEST WITH STATUS CODE 
     app.post('/api/users', (req, res) => {
     const newUser = { id: users.length + 1, ...req.body };
     users.push(newUser);
