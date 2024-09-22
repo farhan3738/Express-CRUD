93893a2e5cd834b2826e39887a7d70ee81ad1471
Muhammad Farhan Elahi <mobicare.pew@gmail.com>
2024-09-22 13:04:08 +0500

    Created basic server and install express

---
+++ index.mjs
@@ -0,0 +1,54 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// let users = [
+//   { id: 1, name: 'Ali', email: 'ali@gmail.com' },
+//   { id: 2, name: 'Janifer', email: 'janifers@gmail.com' },
+// ];
+
+// app.get('/api/users', (req, res) => {
+//     res.status(200).json(users);    
+//   });
+
+//     app.post('/api/users', (req, res) => {
+//     const newUser = { id: users.length + 1, ...req.body };
+//     users.push(newUser);
+//     res.status(201).json(newUser);
+//   });
+
+
+//   app.put('/api/users/:id', (req, res) => {
+//     const userId = parseInt(req.params.id);
+//     const index = users.findIndex(u => u.id === userId);
+    
+//     if (index !== -1) {
+//       users[index] = { id: userId, ...req.body };
+//       res.json(users[index]);
+//     } else {
+//       res.status(404).send('User not found');
+  
+//     }
+//   });
+
+
+ 
+//   app.delete('/api/users/:id', (req, res) => {
+//     const userId = parseInt(req.params.id);
+//     const index = users.findIndex(u => u.id === userId);
+    
+//     if (index !== -1) {
+//       users.splice(index, 1);
+//       res.json({ message: 'User deleted successfully' });
+//     } else {
+//       res.status(404).send('User not found');
+//     }
+//   });
+  
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
